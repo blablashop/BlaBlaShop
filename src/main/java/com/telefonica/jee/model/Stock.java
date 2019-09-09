e4e0c1070cb28ea97c51d40a79e9fd350bbd7387
victor <victor@192.168.1.11>
2019-09-09 11:18:14 +0200

    Clases Unidades y Stocj

--- src/main/java/com/telefonica/jee/model/Stock.java
+++ src/main/java/com/telefonica/jee/model/Stock.java
@@ -1,5 +1,26 @@
 package com.telefonica.jee.model;
 
-public class Stock {
+import java.io.Serializable;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "producto")
+@NamedQuery(name = "Stock.findAll", query = "SELECT d FROM Producto d")
+
+public class Stock implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "cantidad")
+	private int cantidad;
+	
+	@Column(name="minimo")
+	private int minimo;
 
 }
--- src/main/java/com/telefonica/jee/model/Unidades.java
+++ src/main/java/com/telefonica/jee/model/Unidades.java
@@ -1,5 +1,51 @@
 package com.telefonica.jee.model;
 
-public class Unidades {
+import java.io.Serializable;
+import java.math.BigDecimal;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "unidades")
+@NamedQuery(name = "Unidades.findAll", query = "SELECT d FROM Producto d")
+
+public class Unidades implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "name")
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "Unidades [id=" + id + ", name=" + name + "]";
+	}
+	
 }
+
+
+
