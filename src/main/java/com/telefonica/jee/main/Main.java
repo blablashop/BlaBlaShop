074504ff8f9818165c3c3fe50abd62d57e4eb8e9
cessariegos <cesargonzalezarias.cga@gmail.com>
2019-09-09 11:24:49 +0200

    Main

--- src/main/java/com/telefonica/jee/main/Main.java
+++ src/main/java/com/telefonica/jee/main/Main.java
@@ -1,10 +1,34 @@
 package com.telefonica.jee.main;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import com.telefonica.jee.model.Producto;
+import com.telefonica.jee.model.Despensa;
+import com.telefonica.jee.model.Categorias;
+import com.telefonica.jee.model.Stock;
+import com.telefonica.jee.model.Unidades;
+import com.telefonica.jee.model.Usuarios;
+
+
+
 public class Main {
+	
+	static EntityManagerFactory emf;
+	static EntityManager manager;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		emf = Persistence.createEntityManagerFactory("BlaBlaShop");
+		
+		
 	}
 
 }
